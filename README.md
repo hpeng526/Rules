06b620a56639c809a745aa1506be7634efa77978
hpeng526 <hpeng526@gmail.com>
2017-12-17 23:41:05 +0800

    rules

--- README.md
+++ README.md
@@ -2,7 +2,5 @@ ad free hosts
 
 ----
 
-* 可恶的运营商，手机app不是走https的都没法用了。决定在路由上搞死他。
-* 配合dnsmasq
-* anyflow-adb regexp 
-    * find `127.0.0.1 (.*)\n` replace `DOMAIN-SUFFIX,$1,REJECT\n`
+* dnsmasq adbhosts
+* anyflow chosts.conf
--- genRule.sh
+++ genRule.sh
@@ -2,5 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 wget -O yhosts –no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/hosts
-
-awk '/^127.0.0.1/{if ($2!="localhost" && $2!="XiaoQiang") print "DOMAIN-SUFFIX,"$2",REJECT"}' $DIR/yhosts >> $DIR/chosts.conf
+awk '/^127.0.0.1/{if ($2!="XiaoQiang") print $0}' $DIR/yhosts > $DIR/adbhosts
+cat $DIR/extra >> $DIR/adbhosts
+awk '/^127.0.0.1/{if ($2!="localhost" && $2!="XiaoQiang") print "DOMAIN-SUFFIX,"$2",REJECT"}' $DIR/adbhosts > $DIR/chosts.conf
+rm -f $DIR/yhosts
