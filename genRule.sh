b2d6b81e5d4286527eb3b8a187353251d9cae833
hpeng526 <hpeng526@gmail.com>
2017-12-24 00:54:31 +0800

    update

--- genRule.sh
+++ genRule.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-wget -O yhosts –no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/hosts
-awk '/^127.0.0.1/{if ($2!="XiaoQiang") print $0}' $DIR/yhosts > $DIR/adbhosts
+curl https://raw.githubusercontent.com/vokins/yhosts/master/hosts -k|awk '/^127.0.0.1/{if ($2!="XiaoQiang") print $0}' > $DIR/adbhosts
 cat $DIR/extra >> $DIR/adbhosts
+
 echo '[Rule]' > $DIR/chosts.conf
 awk '/^127.0.0.1/{if ($2!="localhost" && $2!="XiaoQiang") print "DOMAIN-SUFFIX,"$2",REJECT"}' $DIR/adbhosts >> $DIR/chosts.conf
-rm -f $DIR/yhosts
+
+echo '[Rule]' > $DIR/chinaunicom.conf
+echo '[Rule]' > $DIR/cmcc.conf
+echo '[Rule]' > $DIR/chinanet.conf
+curl https://raw.githubusercontent.com/huanz/surge-hosts/master/ChinaUnicom.conf -k|awk '/^DOMAIN/{print $0}' >> $DIR/chinaunicom.conf
+curl https://raw.githubusercontent.com/huanz/surge-hosts/master/CMCC.conf -k|awk '/^DOMAIN/{print $0}' >> $DIR/cmcc.conf
+curl https://raw.githubusercontent.com/huanz/surge-hosts/master/ChinaNet.conf -k|awk '/^DOMAIN/{print $0}' >> $DIR/chinanet.conf
