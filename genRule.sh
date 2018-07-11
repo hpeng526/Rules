40220b580082469929f2d65ff0071aa3626ee4e3
hpeng526 <hpeng526@gmail.com>
2018-07-11 10:33:35 +0800

    update

--- genRule.sh
+++ genRule.sh
@@ -7,9 +7,5 @@ cat $DIR/extra >> $DIR/adbhosts
 echo '[Rule]' > $DIR/chosts.conf
 awk '/^127.0.0.1/{if ($2!="localhost" && $2!="XiaoQiang") print "DOMAIN-SUFFIX,"$2",REJECT"}' $DIR/adbhosts >> $DIR/chosts.conf
 
-echo '[Rule]' > $DIR/chinaunicom.conf
-echo '[Rule]' > $DIR/cmcc.conf
-echo '[Rule]' > $DIR/chinanet.conf
-curl https://raw.githubusercontent.com/huanz/surge-hosts/master/ChinaUnicom.conf -k|awk '/^DOMAIN/{print $0}' >> $DIR/chinaunicom.conf
-curl https://raw.githubusercontent.com/huanz/surge-hosts/master/CMCC.conf -k|awk '/^DOMAIN/{print $0}' >> $DIR/cmcc.conf
-curl https://raw.githubusercontent.com/huanz/surge-hosts/master/ChinaNet.conf -k|awk '/^DOMAIN/{print $0}' >> $DIR/chinanet.conf
+echo '[Rule]' > $DIR/adb.conf
+curl https://raw.githubusercontent.com/huanz/surge-hosts/master/surge.conf -k|awk '/^DOMAIN/{print $0}' >> $DIR/adb.conf
